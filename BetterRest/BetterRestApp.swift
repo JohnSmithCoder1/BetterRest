00e638200f7dc471fbab6254a740da7a34587cfb
John Smith <johnsmithcoder1@gmail.com>
2022-12-22 19:48:17 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by J S on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
